0b204c8be3e66bdc0b90cfc9c7aaaa68a5086b21
Mike Murrietta <mmurrietta8@gmail.com>
2022-10-09 19:14:42 -0700

    add basic solution for ex1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,9 @@
 import requests
+import os
+import zipfile
+from io import BytesIO
+import logging
+logger = logging.getLogger(__name__)
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -10,10 +15,32 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
 
+out_folder = 'downloads'
+
 
 def main():
-    # your code here
-    pass
+    # make downloads folder if not existing
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+    
+    # download each file to downloads
+    for download_uri in download_uris:
+        # make request
+        try:
+            logger.info('Downloading file')
+            resp = requests.get(download_uri)
+        except Exception as e:
+            logger.error(f'Failed to download {download_uri}, skipping\n{e}')
+            continue
+
+        # unzip the content, then write to out_folder
+        try:
+            logger.info('Unzipping file')
+            with zipfile.ZipFile(BytesIO(resp.content)) as z:
+                z.extractall(out_folder)
+        except Exception as e:
+            logger.error(f'Failed to extract {download_uri}, skipping\n{e}')
+            continue
 
 
 if __name__ == '__main__':
